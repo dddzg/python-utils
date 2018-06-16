08fc8009cfeb6e14e0b3516b51a6edbb915b5be7
dddzg <240936337@qq.com>
2018-06-17 01:44:36 +0800

    awesome timer

---
+++ utils.py
@@ -0,0 +1,13 @@
+from contextlib import contextmanager
+import time
+@contextmanager
+def timer(name):
+  """
+  Taken from Konstantin Lopuhin https://www.kaggle.com/lopuhin
+  in script named : Mercari Golf: 0.3875 CV in 75 LOC, 1900 s
+  https://www.kaggle.com/lopuhin/mercari-golf-0-3875-cv-in-75-loc-1900-s
+  """
+  t0 = time.time()
+  yield
+  print(f'[{name}] done in {time.time() - t0:.0f} s')
+
