b6d9c4bfe7ef278da4af6c18ae7679e022ff6b59
dddzg <240936337@qq.com>
2018-06-17 01:44:52 +0800

    useful compose

--- utils.py
+++ utils.py
@@ -11,3 +11,14 @@ def timer(name):
   yield
   print(f'[{name}] done in {time.time() - t0:.0f} s')
 
+from functools import reduce
+def compose(*funcs):
+    """Compose arbitrarily many functions, evaluated left to right.
+
+    Reference: https://mathieularose.com/function-composition-in-python/
+    """
+    # return lambda x: reduce(lambda v, f: f(v), funcs, x)
+    if funcs:
+        return reduce(lambda f, g: lambda *a, **kw: g(f(*a, **kw)), funcs)
+    else:
+        raise ValueError('Composition of empty sequence not supported.')
